99752023278e86c241f17fbb21374998600448ef
Ubuntu <ubuntu@ip-172-31-41-157.ap-northeast-1.compute.internal>
2020-03-02 06:43:03 +0000

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
+
+
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+         Schema::defaultStringLength(191);
+          if (\App::environment('production')) {
+            \URL::forceScheme('https');
+          }
     }
 }
